dd94661611b0369c4b20dd1436a6819388267681
Guguloth Srikanth <gugulothsrikanth299@gmail.com>
2025-10-29 16:38:19 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // 👈 enables static export (replaces next export)
+  images: {
+    unoptimized: true, // 👈 required if you use <Image> components
+  },
 };
 
 export default nextConfig;
